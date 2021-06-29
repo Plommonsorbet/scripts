698660494ebb78a01a5eef43293c9cf63fb787de
Isak Johansson <isak@irq1.se>
2021-06-29 13:17:12 +0200

    Separated darwin and linux scripts

--- flake.nix
+++ flake.nix
@@ -11,24 +11,50 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        scripts = pkgs.stdenv.mkDerivation {
-          name = "plommonsorbet-scripts";
+        linux-scripts = pkgs.stdenv.mkDerivation {
+          name = "linux-scripts";
           src = self;
           installPhase = ''
-            mkdir -p $out/bin
-              install -Dm755 $src/bin/* -t $out/bin
+		find $src/bin -type f -maxdepth 1  | xargs -I _ install -Dm755 _ -t $out/bin
+		find $src/bin/linux -type f -maxdepth 1  | xargs -I _ install -Dm755 _ -t $out/bin
               '';
         };
+        darwin-scripts = pkgs.stdenv.mkDerivation {
+          name = "darwin-scripts";
+          src = self;
+          installPhase = ''
+		find $src/bin -type f -maxdepth 1  | xargs -I _ install -Dm755 _ -t $out/bin
+		find $src/bin/darwin -type f -maxdepth 1  | xargs -I _ install -Dm755 _ -t $out/bin
+              '';
+        };
+        common-scripts = pkgs.stdenv.mkDerivation {
+          name = "common-scripts";
+          src = self;
+          installPhase = ''
+		find $src/bin -type f -maxdepth 1  | xargs -I _ install -Dm755 _ -t $out/bin
+              '';
+        };
+
+        all-scripts = pkgs.stdenv.mkDerivation {
+          name = "common-scripts";
+          src = self;
+          installPhase = ''
+		find $src/bin -type f  | xargs -I _ install -Dm755 _ -t $out/bin
+              '';
+        };
+
       in rec {
         packages = {
-          "plommonsorbet-scripts" = scripts;
+          "linux-scripts" = linux-scripts;
+          "darwin-scripts" = darwin-scripts;
+          "common-scripts" = common-scripts;
         };
 
         # By default run the dev version
-        defaultPackage = self.packages.${system}.plommonsorbet-scripts;
+        defaultPackage = self.packages.${system}.common-scripts;
 
         devShell = pkgs.mkShell {
-          buildInputs = [ scripts ];
+          buildInputs = [ all-scripts ];
         };
       });
 
