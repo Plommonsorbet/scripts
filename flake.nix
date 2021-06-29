df3cb0fc3903a3c5c005af9931453cea3ed01883
Isak Johansson <isak@irq1.se>
2021-06-29 12:31:06 +0200

    Clean up and add flake-utils

--- flake.nix
+++ flake.nix
@@ -1,21 +1,36 @@
 {
   description = "Flake of my personal scripts";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
-
-  outputs = { self, nixpkgs }: {
-
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-
-      stdenv.mkDerivation {
-        name = "plommonsorbet-scripts";
-        src = self;
-        installPhase = ''
-          mkdir -p $out/bin
-            install -Dm755 $src/bin/* -t $out/bin
-            '';
-      };
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils  }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        scripts = pkgs.stdenv.mkDerivation {
+          name = "plommonsorbet-scripts";
+          src = self;
+          installPhase = ''
+            mkdir -p $out/bin
+              install -Dm755 $src/bin/* -t $out/bin
+              '';
+        };
+      in rec {
+        packages = {
+          "plommonsorbet-scripts" = scripts;
+        };
+
+        # By default run the dev version
+        defaultPackage = self.packages.${system}.plommonsorbet-scripts;
+
+        devShell = pkgs.mkShell {
+          buildInputs = [ scripts ];
+        };
+      });
+
 }
+
