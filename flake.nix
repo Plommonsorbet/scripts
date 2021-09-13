6b8eb087d466f9e34534c51db2c0ec9482d29b11
Isak Johansson <isak@irq1.se>
2021-09-14 00:23:30 +0200

    UPDATE Name of package

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       devShell.${system} = pkgs.mkShell { buildInputs = [ scripts ]; };
 
       overlay = final: prev: {
-      	plommonsorbet-scripts = scripts;
+      	scripts = scripts;
       };
 
     };
