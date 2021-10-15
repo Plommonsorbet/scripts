ffac65ba334aefde207eb49625ce6c4f2b205adc
Isak Johansson <isak@irq1.se>
2021-10-15 18:16:14 +0200

    UPDATE Some new programs

--- shell.nix
+++ shell.nix
@@ -1,9 +1,7 @@
-with import <nixpkgs> { };
-
-stdenv.mkDerivation {
-  name = "scripts-dev-shell";
-  #buildInputs = [ ];
-  shellHook = ''
-            export PATH="$(git rev-parse --show-toplevel)/bin:$PATH"
-  '';
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+) {
+  src =  ./.;
+}).shellNix
